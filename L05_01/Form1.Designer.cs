4cb8ba02a1c830f973a9c40f7ee18e512e0e6963
Ian Smith <imsmith1995@gmail.com>
2023-06-30 15:24:54 -0400

    If statements added

--- L05_01/Form1.Designer.cs
+++ L05_01/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.chkPowerWindows = new System.Windows.Forms.CheckBox();
             this.chkAirConditioning = new System.Windows.Forms.CheckBox();
             this.chkChrome = new System.Windows.Forms.CheckBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblHeader
@@ -48,6 +50,8 @@
             // chkPowerWindows
             // 
             this.chkPowerWindows.AutoSize = true;
+            this.chkPowerWindows.Checked = true;
+            this.chkPowerWindows.CheckState = System.Windows.Forms.CheckState.Indeterminate;
             this.chkPowerWindows.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chkPowerWindows.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.chkPowerWindows.Location = new System.Drawing.Point(106, 269);
@@ -60,6 +64,8 @@
             // chkAirConditioning
             // 
             this.chkAirConditioning.AutoSize = true;
+            this.chkAirConditioning.Checked = true;
+            this.chkAirConditioning.CheckState = System.Windows.Forms.CheckState.Indeterminate;
             this.chkAirConditioning.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chkAirConditioning.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.chkAirConditioning.Location = new System.Drawing.Point(106, 342);
@@ -72,6 +78,8 @@
             // chkChrome
             // 
             this.chkChrome.AutoSize = true;
+            this.chkChrome.Checked = true;
+            this.chkChrome.CheckState = System.Windows.Forms.CheckState.Indeterminate;
             this.chkChrome.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chkChrome.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.chkChrome.Location = new System.Drawing.Point(106, 418);
@@ -81,12 +89,36 @@
             this.chkChrome.Text = "Chrome";
             this.chkChrome.UseVisualStyleBackColor = true;
             // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(90, 509);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(388, 81);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Price";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblOutput.Location = new System.Drawing.Point(564, 548);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(127, 42);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.Text = "(Price)";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(767, 615);
+            this.ClientSize = new System.Drawing.Size(773, 754);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.chkChrome);
             this.Controls.Add(this.chkAirConditioning);
             this.Controls.Add(this.chkPowerWindows);
@@ -104,6 +136,8 @@
         private System.Windows.Forms.CheckBox chkPowerWindows;
         private System.Windows.Forms.CheckBox chkAirConditioning;
         private System.Windows.Forms.CheckBox chkChrome;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
--- L05_01/Form1.cs
+++ L05_01/Form1.cs
@@ -16,5 +16,58 @@ namespace L05_01
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Defining variables to be used
+            int baseCost = 25000;
+            int windowsCost = 1000;
+            int airCost = 1500;
+            int chromeCost = 2000;
+            int outputCost = baseCost;
+            string outputString = string.Empty;
+
+            //If statements modifying the total cost based upon check boxes.
+            if (chkPowerWindows.Checked )
+            {
+                outputCost += windowsCost;
+            }
+
+            if (chkAirConditioning.Checked )
+            {
+                outputCost += airCost;
+            }
+
+            if (chkChrome.Checked )
+            {
+                outputCost += chromeCost;
+            }
+
+            //Displaying the total cost formatted as currency.
+            outputString = outputCost.ToString("C");
+            lblOutput.Text = outputString;
+
+            //If statements ensuring user either checks or unchecks boxes.
+            if (chkPowerWindows.CheckState == CheckState.Indeterminate)
+            {
+                MessageBox.Show("you must make a choice for Power Windows",
+                        "Missing Entry");
+                return;
+            }
+
+            if (chkAirConditioning.CheckState == CheckState.Indeterminate)
+            {
+                MessageBox.Show("you must make a choice for Air Conditioning",
+                        "Missing Entry");
+                return;
+            }
+
+            if (chkChrome.CheckState == CheckState.Indeterminate)
+            {
+                MessageBox.Show("you must make a choice for Chrome",
+                        "Missing Entry");
+                return;
+            }
+        }
     }
 }
